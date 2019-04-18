5141e9f00c1c5d149d297b013b09313e26c7282f
Chris Arnesen <chris@alwaysai.co>
2019-04-18 14:55:09 -0700

    Fix unit tests

--- src/prompt.test.ts
+++ src/prompt.test.ts
@@ -1,9 +1,12 @@
-import { prompt } from './prompt';
 import { runAndCatch } from '@alwaysai/always-cli';
+
+import { prompt } from './prompt';
 
 describe(prompt.name, () => {
   it('throws "This feature is disabled" if process.stdin is not a TTY', async () => {
-    const ex = await runAndCatch(prompt, []);
-    expect(ex.message).toMatch(/This feature is disabled/i);
+    if (process.stdin.isTTY) {
+      const ex = await runAndCatch(prompt, []);
+      expect(ex.message).toMatch(/This feature is disabled/i);
+    }
   });
 });
