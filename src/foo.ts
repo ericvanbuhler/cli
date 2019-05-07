8a4afcdc59f832446aab0f78afc7c5a592b4edc7
Chris Arnesen <chris@alwaysai.co>
2019-05-07 09:22:56 -0700

    Uncomment foo

--- src/foo.ts
+++ src/foo.ts
@@ -24,36 +24,36 @@ cognitoUser.forgotPassword({
   onSuccess: console.log,
 });
 
-// cognitoUser.authenticateUser(authenticationDetails, {
-//   onSuccess(result) {
-//     const accessToken = result.getAccessToken().getJwtToken();
-//     console.log('result');
-//     console.log(result);
-//     AWS.config.region = 'us-east-1';
-//     // refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
+cognitoUser.authenticateUser(authenticationDetails, {
+  onSuccess(result) {
+    const accessToken = result.getAccessToken().getJwtToken();
+    console.log('result');
+    console.log(result);
+    AWS.config.region = 'us-east-1';
+    // refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
 
-//     // AWS.config.credentials.refresh(error => {
-//     //   if (error) {
-//     //     console.error(error);
-//     //   } else {
-//     //     // Instantiate aws sdk service objects now that the credentials have been updated.
-//     //     // example: var s3 = new AWS.S3();
-//     //     console.log('Successfully logged!');
-//     //   }
-//     // });
-//   },
-//   newPasswordRequired(...args) {
-//     cognitoUser.completeNewPasswordChallenge('alwaysai', [], {
-//       onSuccess(...args) {
-//         console.log(args);
-//       },
-//       onFailure(ex) {
-//         console.log(ex);
-//       },
-//     });
-//     console.log(args);
-//   },
-//   onFailure(err) {
-//     console.log(err);
-//   },
-// });
+    // AWS.config.credentials.refresh(error => {
+    //   if (error) {
+    //     console.error(error);
+    //   } else {
+    //     // Instantiate aws sdk service objects now that the credentials have been updated.
+    //     // example: var s3 = new AWS.S3();
+    //     console.log('Successfully logged!');
+    //   }
+    // });
+  },
+  newPasswordRequired(...args) {
+    cognitoUser.completeNewPasswordChallenge('alwaysai', [], {
+      onSuccess(...args) {
+        console.log(args);
+      },
+      onFailure(ex) {
+        console.log(ex);
+      },
+    });
+    console.log(args);
+  },
+  onFailure(err) {
+    console.log(err);
+  },
+});
