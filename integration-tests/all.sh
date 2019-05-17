42c1f4b85c62c6760b2a70e9022f4988e39889e3
Chris Arnesen <chris@alwaysai.co>
2019-05-17 13:37:22 -0700

    Update all.sh

--- integration-tests/all.sh
+++ integration-tests/all.sh
@@ -4,8 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 set -o errexit
 set -o xtrace
 
-tsc
-rm alwaysai.app.json
+rm -f alwaysai.app.json
 
 alwaysai app init --yes
 alwaysai app models add alwaysai/agenet
