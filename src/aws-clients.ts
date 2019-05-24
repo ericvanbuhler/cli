6424e55470dd0c48f197643e926818593235c0ee
Chris Arnesen <chris.arnesen@gmail.com>
2019-05-24 15:47:31 -0700

    Remove API keys (#27)

--- src/aws-clients.ts
+++ src/aws-clients.ts
@@ -3,7 +3,11 @@ import * as Aws from 'aws-sdk';
 export function S3(opts: { region: string }) {
   return new Aws.S3({
     region: opts.region,
-    accessKeyId: 'AKIA6PDW4RG4KZOCN2V7',
-    secretAccessKey: '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',
+    ...JSON.parse(
+      Buffer.from(
+        'eyJhY2Nlc3NLZXlJZCI6IkFLSUE2UERXNFJHNE9KTkNLUEdCIiwic2VjcmV0QWNjZXNzS2V5IjoiSmt0QWttUVhKU1ppdGlucjZQZGw1VGIyOXIvWEltVHFSVXUzcDErZyJ9',
+        'base64',
+      ).toString('utf-8'),
+    ),
   });
 }
